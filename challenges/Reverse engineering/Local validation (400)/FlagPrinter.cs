e1a49f0531d6e080ca141cccb3dac0264c68a755
najashark <binpda.shark@gmail.com>
2018-09-20 19:36:27 +0800

    add local validation writeup

---
+++ challenges/Reverse engineering/Local validation (400)/FlagPrinter.cs
@@ -0,0 +1,49 @@
+﻿namespace FlagPrinter
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    /// <summary>Contains native method declarations.</summary>
+    internal static class NativeMethods
+    {
+        /// <summary>Checks the validity of the specified password.</summary>
+        /// <param name="password">The password to validate.</param>
+        /// <returns>0 if the password is valid, otherwise non-zero.</returns>
+        [DllImport("PasswordValidation")]
+        internal static extern int IsPasswordValid(string password);
+    }
+
+    /// <summary>Contains the main entry point of the application.</summary>
+    public class Program
+    {
+        /// <summary>The flag for the challenge :)</summary>
+        private const string Flag = "";  // not that easy...
+
+        /// <summary>Main entry point of the application.</summary>
+        public static void Main()
+        {
+            while (true)
+            {
+                // read the user's password from stdin
+                Console.Write("Enter password: ");
+                var password = Console.ReadLine();
+
+                // validate it using the secure authentication library
+                Console.Write("Authentication: ");
+                if (NativeMethods.IsPasswordValid(password) == 0)
+                {
+                    // the password was authenticated successfully
+                    Console.WriteLine("ACCEPTED!\n");
+                    break;
+                }
+
+                // the password was rejected
+                Console.WriteLine("REJECTED!\n");
+            }
+
+            Console.WriteLine("Congratulations!");
+            Console.WriteLine("The flag is \"{0}\".", Flag);
+            Console.ReadKey();  // don't quit until after a key is pressed
+        }
+    }
+}
--- challenges/Reverse engineering/Local validation (400)/readme.txt
+++
@@ -1,5 +0,0 @@
-Get the flag
-file
-5 point hint: You don't need to understand how either of the files work, just the interaction between them.
-20 point hint: Create your own DLL that exports an IsPasswordValid() function, and attempt to get the function signature correct.
-
